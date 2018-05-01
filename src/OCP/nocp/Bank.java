6aaed872993ebe9a8873a2e371ceec1db3f2a2df
iyeongjun <they6687@naver.com>
2018-05-01 22:20:17 +0900

    add Sopt2

--- src/OCP/nocp/Bank.java
+++ src/OCP/nocp/Bank.java
@@ -1,5 +1,10 @@
 package OCP.nocp;
 
+/**
+ * 개방 폐쇄의 원칙
+ * 기능확장에는 열려있고, 기능 수정에는 닫혀있어야한다
+ * 데이터를 직접주는게 아닌, 기능으로 제공하면됩니다.
+ */
 public class Bank {
     BankUser bankUser = new BankUser();
     public void checkPromotion(){
